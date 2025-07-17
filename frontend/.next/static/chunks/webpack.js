8f7803209380a38a27aeff2d8516c0feb3625a11
prateeka0507 <prateekakathiresan@gmail.com>
2025-07-17 18:59:00 +0530

    ui for contact , about and home

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f2992824336bc89"; }
+/******/ 		__webpack_require__.h = function() { return "da0f2b061fe67871"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.b7a94948ac643b27.hot-update.js
+++ frontend/.next/static/webpack/webpack.b7a94948ac643b27.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1f2992824336bc89"; }
+/******/ 	__webpack_require__.h = function() { return "4c8678dfd0e102fa"; }
 /******/ }();
 /******/ 
 /******/ }
